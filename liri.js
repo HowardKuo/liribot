086ec99944e4f9d1408949439b72e20336183f59
HowardKuo <howardkuo91@gmail.com>
2019-05-03 23:56:55 -0700

    variable and functions created

--- liri.js
+++ liri.js
@@ -1,4 +1,45 @@
 //api key for omdb: 5d673c51
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
+
+var searchType = process.argv[2];
+var searchTerm = process.argv.slice(3).join(" ");
+
+function typeOfSearch(searchType) {
+    switch (searchType) {
+        case "concert-this":
+            getConcert();
+            break;
+
+        case "spotify-this-song":
+            getSong();
+            break;
+        
+        case "movie-this":
+            getMovie();
+            break;
+        
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+    }
+
+    function getConcert() {
+
+    }
+
+    function getSong() {
+
+    }
+
+    function getMovie() {
+
+    }
+
+    function doWhatItSays() {
+        
+    }
+}
