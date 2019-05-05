5846fdc200b866df13b9b85ad9fcd7af18f8dc28
HowardKuo <howardkuo91@gmail.com>
2019-05-04 21:06:31 -0700

    get Concert and addLog functions completed

--- liri.js
+++ liri.js
@@ -1,12 +1,15 @@
 //api key for omdb: 5d673c51
 require("dotenv").config();
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 var axios = require("axios");
 var fs = require("fs");
-var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+// var spotify = new Spotify(keys.spotify);
 
 var searchType = process.argv[2];
 var searchTerm = process.argv.slice(3).join(" ");
+
+typeOfSearch(searchType);
 
 function typeOfSearch(searchType) {
     switch (searchType) {
@@ -28,8 +31,20 @@ function typeOfSearch(searchType) {
     }
 
     function getConcert() {
-
-    }
+        var URL = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
+        axios.get(URL).then(function (response) {
+            var jsonData = response.data;
+            for (var i = 0; i < jsonData.length; i++) {
+                var showData = [
+                "Venue: " + jsonData[i].venue.name,
+                "City: " + jsonData[i].venue.city,
+                "Date: " + moment(jsonData[i].datetime).format('MM/DD/YYYY'),
+                "\n--------------------------------------------------------\n"
+                ].join("\n");
+                addLog(showData);
+            }
+          });
+        };
 
     function getSong() {
 
@@ -40,6 +55,12 @@ function typeOfSearch(searchType) {
     }
 
     function doWhatItSays() {
-        
+
     }
+    function addLog(data) {
+        fs.appendFile("log.txt", data, function (err) {
+          if (err) throw err;
+          console.log(data);
+        });
+      };
 }
